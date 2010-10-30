388a0274a1337d53a7d78ea53114593ff23fcb58
mej <mej@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2010-10-30 08:30:46 +0000

    Sat Oct 30 01:30:46 2010                        Michael Jennings (mej)
    Add copyrights to perl modules.
    ----------------------------------------------------------------------
    
    
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@45 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- common/lib/Warewulf/Daemon.pm
+++ common/lib/Warewulf/Daemon.pm
@@ -1,3 +1,22 @@
+# Copyright (c) 2003-2010, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; either version 2 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+# for more details.
+#
+# The GNU GPL Document can be found at:
+# http://www.gnu.org/copyleft/gpl.html
+#
+# $Id$
+#
 
 package Warewulf::Daemon;
 
@@ -7,10 +26,7 @@ use Exporter;
 
 our @ISA = ('Exporter');
 
-our @EXPORT = qw (
-    &daemonize
-    &daemon_check
-);
+our @EXPORT = ('&daemonize', '&daemon_check');
 
 my $DAEMONIZED;
 
