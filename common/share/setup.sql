cff2afb47e898565afc4e1ed4d9657adc25bfe4b
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2010-12-08 21:25:20 +0000

    Some test data to be put into the DB.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@72 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- common/share/setup.sql
+++ common/share/setup.sql
@@ -27,11 +27,46 @@ CREATE TABLE lookup
     field VARCHAR(64),
     value VARCHAR(64),
     object_id INT,
-    active BOOL,
     FOREIGN KEY (object_id) REFERENCES datastore (id),
     INDEX (id),
     PRIMARY KEY (id)
 ) ENGINE=INNODB;
+
+
+
+
+INSERT INTO datastore (serialized) VALUES ("serialized data for a group object named: group1");
+INSERT INTO datastore (serialized) VALUES ("serialized data for a group object named: interactive");
+INSERT INTO datastore (serialized) VALUES ("serialized data for a group object named: io");
+INSERT INTO datastore (serialized) VALUES ("serialized data for a rack object named: rack1");
+INSERT INTO datastore (serialized) VALUES ("serialized data for a rack object named: rack2");
+INSERT INTO datastore (serialized) VALUES ("serialized data for a node object named: n0000");
+INSERT INTO datastore (serialized) VALUES ("serialized data for a node object named: n0001");
+INSERT INTO datastore (serialized) VALUES ("serialized data for a node object named: n0002");
+
+INSERT INTO lookup (type, field, value, object_id) VALUES ("group",     "name",     "group1",       "1");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("group",     "name",     "interactive",  "2");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("group",     "name",     "io",           "3");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("rack",      "name",     "rack1",        "4");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("rack",      "name",     "rack2",        "5");
+
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "name",     "n0000",        "6");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "group1",       "6");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "interactive",  "6");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "rack",     "rack1",        "6");
+
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "name",     "n0001",        "7");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "group1",       "7");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "interactive",  "7");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "rack",     "rack1",        "7");
+
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "name",     "n0002",        "8");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "group1",       "8");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "io",           "8");
+INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "rack",     "rack2",        "8");
+
+
+
 
 
 
