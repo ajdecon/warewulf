b5c84f0ef3efb9092d5230398b2de34523540a5c
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2010-12-24 07:19:02 +0000

    Enabled node communication between initramfs, cgi/mod_perl interface, and backend datastore. WAHOO!
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@131 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- provision/cgi-bin/nodeinterface.pl
+++ provision/cgi-bin/nodeinterface.pl
@@ -1,11 +1,35 @@
 #!/usr/bin/perl
 
 use CGI;
+use Warewulf::DB;
+use Warewulf::ObjectFactory;
 
 my $q = CGI->new();
+my $db = Warewulf::DB->new();
 
 print $q->header();
 
-print "echo Hello World\n";
+my $hwaddr = $q->param('hwaddr');
 
+
+my $ethernetSet = $db->get_objects("ethernet", "name", $hwaddr);
+my $eth = $ethernetSet->get_object(0);
+
+if ($eth) {
+    my $ethName = $eth->get("name");
+    my $nodeSet = $db->get_objects("node", "hwaddr", $ethName);
+    my $node = $nodeSet->get_object(0);
+    if ($node) {
+        my $nodeName = $node->get("name");
+        print "echo Welcome back Mr. $nodeName\n";
+    } else {
+        print "echo Welcome back Mr. $ethName\n";
+    }
+} else {
+    my $newnode = Warewulf::ObjectFactory->new("ethernet");
+    $newnode->set("name", $hwaddr);
+    $db->persist($newnode);
+
+    print "echo Hello Mr. $hwaddr, I have added you to my datastore\n";
+}
 
