fb28baaee04aee6688725caabaa1b48d29a91928
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2010-12-25 04:47:47 +0000

    Removing Object::Ethernet type.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@136 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- common/lib/Warewulf/ObjectFactory.pm
+++ common/lib/Warewulf/ObjectFactory.pm
@@ -15,7 +15,6 @@ use Warewulf::Logger;
 use Warewulf::Config;
 use Warewulf::Object::Node;
 use Warewulf::Object::Vnfs;
-use Warewulf::Object::Ethernet;
 use DBI;
 
 =head1 NAME
@@ -49,8 +48,6 @@ new($$)
         return(Warewulf::Object::Node->new(@_));
     } elsif ($type eq "VNFS") {
         return(Warewulf::Object::Vnfs->new(@_));
-    } elsif ($type eq "ETHERNET") {
-        return(Warewulf::Object::Ethernet->new(@_));
     } else {
         &eprint("Unknown object Type: $type\n");
         exit 1;
