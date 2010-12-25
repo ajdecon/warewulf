971624ade98d2cc0fc6959b10ee4aa305272bb55
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2010-12-25 04:44:50 +0000

    Updates to nodeinterface.pl
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@135 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- provision/cgi-bin/nodeinterface.pl
+++ provision/cgi-bin/nodeinterface.pl
@@ -12,24 +12,18 @@ print $q->header();
 my $hwaddr = $q->param('hwaddr');
 
 
-my $ethernetSet = $db->get_objects("ethernet", "name", $hwaddr);
-my $eth = $ethernetSet->get_object(0);
+my $nodeSet = $db->get_objects("node", "hwaddr", $hwaddr);
+my $node = $nodeSet->get_object(0);
 
-if ($eth) {
-    my $ethName = $eth->get("name");
-    my $nodeSet = $db->get_objects("node", "hwaddr", $ethName);
-    my $node = $nodeSet->get_object(0);
-    if ($node) {
-        my $nodeName = $node->get("name");
-        print "echo Welcome back Mr. $nodeName\n";
-    } else {
-        print "echo Welcome back Mr. $ethName\n";
-    }
+if ($node) {
+    my $nodeName = $node->get("name");
+    print "echo Welcome back $nodeName\n";
 } else {
-    my $newnode = Warewulf::ObjectFactory->new("ethernet");
-    $newnode->set("name", $hwaddr);
+    my $newnode = Warewulf::ObjectFactory->new("node");
+    $newnode->set("name", "newnode");
+    $newnode->set("hwaddr", $hwaddr);
     $db->persist($newnode);
 
-    print "echo Hello Mr. $hwaddr, I have added you to my datastore\n";
+    print "echo Hello newnode, I have added you to my datastore\n";
 }
 
