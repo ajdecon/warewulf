dbb98a564732a3d53a6881fc8c77375dbc9f1efa
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-01-15 04:56:57 +0000

    iterate through all node object values and subsitute shell variables as they are found in the node scripts.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@187 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- provision/cgi-bin/nodeinterface.pl
+++ provision/cgi-bin/nodeinterface.pl
@@ -11,19 +11,27 @@ print $q->header();
 
 my $hwaddr = $q->param('hwaddr');
 
-
 my $nodeSet = $db->get_objects("node", "hwaddr", $hwaddr);
 my $node = $nodeSet->get_object(0);
+my %nhash = $node->get_hash();
 
 if ($node) {
     my $nodeName = $node->get("name");
-    print "echo Welcome back $nodeName\n";
 } else {
-    my $newnode = Warewulf::ObjectFactory->new("node");
-    $newnode->set("name", "newnode");
-    $newnode->set("hwaddr", $hwaddr);
-    $db->persist($newnode);
-
-    print "echo Hello newnode, I have added you to my datastore\n";
+    $node = Warewulf::ObjectFactory->new("node");
+    $node->set("name", "newnode");
+    $node->set("hwaddr", $hwaddr);
+    $db->persist($node);
 }
 
+
+foreach my $script ($node->get("bootscript")) {
+    my $s = $db->get_objects("script", "name", $script);
+    my $sobj = $s->get_object(0);
+    my $script = $db->get_data($sobj->get("id"));
+    foreach my $key (keys %nhash) {
+        my $uc_key = uc($key);
+        $script =~ s/\$\{?$uc_key\}?/$nhash{$key}/g;
+    }
+    print $script;
+}
