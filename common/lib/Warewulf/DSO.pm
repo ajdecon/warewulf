c6ba6077783d1cf01fc9694421f901529f6d7c86
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-02-07 22:04:17 +0000

    Created DSO (Data Store Object) base class structure.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@213 3fa55bd7-8392-0410-b022-a3fc164e7bfc

---
+++ common/lib/Warewulf/DSO.pm
@@ -0,0 +1,106 @@
+# Copyright (c) 2001-2003 Gregory M. Kurtzer
+#
+# Copyright (c) 2003-2011, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+#
+# $Id: DSO.pm 50 2010-11-02 01:15:57Z gmk $
+#
+
+package Warewulf::DSO;
+
+use Warewulf::Object;
+
+our @ISA = ('Warewulf::Object');
+
+=head1 NAME
+
+Warewulf::DSO - Warewulf's DSO (Data Store Object) base class
+
+=head1 ABOUT
+
+
+=head1 SYNOPSIS
+
+    use Warewulf::DSO;
+
+    my $obj = Warewulf::DSO->new();
+
+
+=head1 METHODS
+
+=over 12
+=cut
+
+
+=item new()
+
+The new constructor will create the object that references configuration the
+stores.
+
+=cut
+sub
+new($$)
+{
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self = ();
+
+    $self = $class->SUPER::new();
+    bless($self, $class);
+
+    return $self->init(@_);
+}
+
+
+=item type()
+
+Return a string that defines this object type as it will be stored in the
+datastore.
+
+=cut
+sub
+type($)
+{
+    my $self = shift;
+    my $type = ref($self);
+    $type =~ s/^.+:://;
+
+    return(lc($type));
+}
+
+
+=item lookups()
+
+Return a list of lookup names for this DSO type.
+
+=cut
+sub
+lookups($)
+{
+    my $self = shift;
+
+    return("NAME");
+}
+
+
+
+=back
+
+=head1 SEE ALSO
+
+Warewulf::Object
+
+=head1 COPYRIGHT
+
+Copyright (c) 2001-2003 Gregory M. Kurtzer
+
+Copyright (c) 2003-2011, The Regents of the University of California,
+through Lawrence Berkeley National Laboratory (subject to receipt of any
+required approvals from the U.S. Dept. of Energy).  All rights reserved.
+
+=cut
+
+
+1;
