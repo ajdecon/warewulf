044179016dd44e4d717d6fedc1dcf93e798f6b91
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-02-11 22:19:50 +0000

    Initial Event interface for DHCP support.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@233 3fa55bd7-8392-0410-b022-a3fc164e7bfc

---
+++ provision/lib/Warewulf/Event/Dhcp.pm
@@ -0,0 +1,36 @@
+# Copyright (c) 2001-2003 Gregory M. Kurtzer
+#
+# Copyright (c) 2003-2011, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+#
+# $Id: Dhcp.pm 50 2010-11-02 01:15:57Z gmk $
+#
+
+package Warewulf::Event::Dhcp;
+
+use Warewulf::Logger;
+use Warewulf::Provision::Dhcp;
+use Warewulf::Event;
+use Warewulf::EventHandler;
+use Warewulf::Provision::DhcpFactory;
+
+
+my $event = Warewulf::EventHandler;
+
+
+sub
+update_dhcp()
+{
+    my $dhcp = Warewulf::Provision::DhcpFactory->new();
+    $dhcp->persist();
+}
+
+
+
+$event->register("node.add", \&update_dhcp);
+$event->register("node.delete", \&update_dhcp);
+$event->register("node.modify", \&update_dhcp);
+
+1;
