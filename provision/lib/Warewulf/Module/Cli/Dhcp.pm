e17c5dff8b39fdb1372f1dfa5ab7fd605d48f8e4
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-02-23 19:09:03 +0000

    Added a wwsh/cli interface for rebuilding the DHCP configuration
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@248 3fa55bd7-8392-0410-b022-a3fc164e7bfc

---
+++ provision/lib/Warewulf/Module/Cli/Dhcp.pm
@@ -0,0 +1,64 @@
+#
+# Copyright (c) 2001-2003 Gregory M. Kurtzer
+#
+# Copyright (c) 2003-2011, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+
+
+
+package Warewulf::Module::Cli::Dhcp;
+
+use Warewulf::Logger;
+use Warewulf::Provision::DhcpFactory;
+
+our @ISA = ('Warewulf::Module::Cli');
+
+
+sub
+new()
+{
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self = {};
+
+    bless($self, $class);
+
+    return $self;
+}
+
+
+sub
+help()
+{
+    my ($self, $keyword) = @_;
+    my $output;
+
+    $output .= "        The DHCP command will reconfigure the DHCP service for this host\n";
+
+    return($output);
+}
+
+
+sub
+exec()
+{
+    my ($self, @args) = @_;
+
+    &nprint("Rebuilding the DHCP configuration\n");
+    my $dhcp = Warewulf::Provision::DhcpFactory->new();
+    $dhcp->persist();
+    &nprint("Done.\n");
+
+}
+
+
+
+1;
+
+
+
+
+
+
