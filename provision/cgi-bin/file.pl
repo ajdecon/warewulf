4c69aceed718e6debd2c17023947ade75ea5b3ef
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-03-12 02:17:20 +0000

    Support adding files to DB and including them in the provisioning process.
    eg:
    
    wwsh file --import /etc/passwd
    wwsh node -s file=passwd
    
    That will include a snapshot/clone of the master's /etc/passwd on the node.
    
    note: This is *NOT* using the live passwd file from the master!!!
    
    
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@283 3fa55bd7-8392-0410-b022-a3fc164e7bfc

---
+++ provision/cgi-bin/file.pl
@@ -0,0 +1,66 @@
+#!/usr/bin/perl
+#
+# Copyright (c) 2001-2003 Gregory M. Kurtzer
+#
+# Copyright (c) 2003-2011, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+
+
+use CGI;
+use Warewulf::DataStore;
+use Warewulf::DSOFactory;
+
+my $q = CGI->new();
+my $db = Warewulf::DataStore->new();
+
+print $q->header("text/plain");
+
+my $hwaddr = $q->param('hwaddr');
+my $fileid = $q->param('fileid');
+
+
+if (! $fileid) {
+    if ($hwaddr =~ /^([a-zA-Z0-9:]+)$/) {
+        $hwaddr = $1;
+
+        my $nodeSet = $db->get_objects("node", "hwaddr", $hwaddr);
+        my $node = $nodeSet->get_object(0);
+
+        if ($node) {
+            my $nodeName = $node->get("name");
+
+            foreach my $file ($node->get("file")) {
+                if (! $file) {
+                    next;
+                }
+                my $objSet = $db->get_objects("file", "name", $file);
+                foreach my $obj ($objSet->get_list()) {
+                    if ($obj) {
+                        printf("%s %s %s %s %s %s\n",
+                            $obj->get("id") || "NULL",
+                            $obj->get("name") || "NULL",
+                            $obj->get("uid") || "0",
+                            $obj->get("gid") || "0",
+                            $obj->get("mode") || "NULL",
+                            $obj->get("path") || "NULL");
+                    }
+                }
+            }
+        }
+    }
+} elsif ($fileid =~ /^([0-9]+)$/ ) {
+    $fileid = $1;
+    my $fileObj = $db->get_objects("file", "id", $fileid)->get_object(0);;
+
+    if ($fileObj) {
+        my $binstore = $db->binstore($fileObj->get("id"));
+        while(my $buffer = $binstore->get_chunk()) {
+            print $buffer;
+        }
+    }
+
+} else {
+    &wprint("FILEID contains illegal characters\n");
+}
