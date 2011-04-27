076bd9c62a53128c9cefa3e792e6c357783d642e
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-04-27 22:54:51 +0000

    Work in progress of extending the functionality of the cli keyword "node".
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@351 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- common/lib/Warewulf/Module/Cli/Node.pm
+++ common/lib/Warewulf/Module/Cli/Node.pm
@@ -16,6 +16,8 @@ use Warewulf::Module::Cli;
 use Warewulf::Term;
 use Warewulf::DataStore;
 use Warewulf::Util;
+use Warewulf::DSO::Netdev;
+use Warewulf::DSO::Node;
 use Getopt::Long;
 use Text::ParseWords;
 
@@ -108,9 +110,15 @@ complete()
         'l|lookup=s'    => \$opt_lookup,
     );
 
+    if (exists($ARGV[1]) and ($ARGV[1] eq "print" or $ARGV[1] eq "create" or $ARGV[1] eq "edit")) {
+        @ret = $db->get_lookups($entity_type, $opt_lookup);
+    } else {
+        @ret = ("print", "create", "edit");
+    }
+
     @ARGV = ();
 
-    return($db->get_lookups($entity_type, $opt_lookup));
+    return(@ret);
 }
 
 sub
@@ -119,11 +127,25 @@ exec()
     my $self = shift;
     my $db = $self->{"DB"};
     my $term = Warewulf::Term->new();
+    my $opt_lookup = "name";
     my $opt_new;
     my $opt_obj_delete;
     my $opt_help;
+    my $opt_hwaddr;
+    my $opt_ipaddr;
+    my $opt_netmask;
+    my $opt_netdev;
+    my $opt_devremove;
+    my $opt_bootstrap;
+    my $opt_vnfs;
+    my @opt_set;
+    my @opt_add;
+    my @opt_del;
     my @opt_print;
     my $return_count;
+    my $objSet;
+    my @changes;
+    my $command;
 
     @ARGV = ();
     push(@ARGV, @_);
@@ -131,37 +153,65 @@ exec()
     GetOptions(
         'n|new'         => \$opt_new,
         'p|print=s'     => \@opt_print,
+        's|set=s'       => \@opt_set,
+        'a|add=s'       => \@opt_add,
+        'd|del=s'       => \@opt_del,
         'DELETE'        => \$opt_obj_delete,
         'h|help'        => \$opt_help,
+        'netdev=s'      => \$opt_netdev,
+        'remove'        => \$opt_devremove,
+        'hwaddr=s'      => \$opt_hwaddr,
+        'ipaddr=s'      => \$opt_ipaddr,
+        'netmask=s'     => \$opt_netmask,
+        'b|bootstrap=s' => \$opt_bootstrap,
+        'v|vnfs=s'      => \$opt_vnfs,
+        'l|lookup=s'    => \$opt_lookup,
+
     );
+
+    if (scalar(@ARGV) > 0) {
+        $command = shift(@ARGV);
+        &dprint("Running command: $command\n");
+    } else {
+        &dprint("Returning with nothing to do\n");
+        return();
+    }
 
     if (! $db) {
         &eprint("Database object not avaialble!\n");
         return();
     }
 
-    if ($opt_new) {
+    if ($command eq "create") {
+        $objSet = Warewulf::ObjectSet->new();
         foreach my $string (&expand_bracket(@ARGV)) {
             my $obj;
             $obj = Warewulf::DSOFactory->new($entity_type);
 
-            $obj->set("name", $string);
+            $obj->set($opt_lookup, $string);
 
-            $db->persist($obj);
+            $objSet->add($obj);
         }
+        $db->persist($objSet);
+    } else {
+        $objSet = $db->get_objects($opt_type || $entity_type, $opt_lookup, &expand_bracket(@ARGV));
     }
 
-    if (@opt_print) {
+    if ($command eq "delete") {
+        $db->del_object($objSet);
+    } elsif ($command eq "print") {
+
         if (scalar(@opt_print) > 0) {
             @opt_print = split(",", join(",", @opt_print));
+        } else {
+            @opt_print = ("name", "cluster", "group", "vnfs", "hwaddr");
         }
-        my $objectSet = $db->get_objects($opt_type || $entity_type, $opt_lookup, &expand_bracket(@ARGV));
-        my @objList = $objectSet->get_list();
-
         if (@opt_print and scalar @opt_print > 1 and $opt_print[0] ne ":all") {
-            &nprintf("%-20s " x (scalar @opt_print) ."\n", map {uc($_);}@opt_print);
+            my $string = sprintf("%-15s " x (scalar @opt_print), map {uc($_);} @opt_print);
+            &nprint($string ."\n");
+            &nprint("=" x length($string) ."\n");
         }
-        foreach my $o ($objectSet->get_list()) {
+        foreach my $o ($objSet->get_list()) {
             my @values;
             if (@opt_print and $opt_print[0] eq ":all") {
                 my %hash = $o->get_hash();
@@ -170,9 +220,23 @@ exec()
                 &nprintf("#### %s %s#\n", $name, "#" x (72 - length($name)));
                 foreach my $h (keys %hash) {
                     if(ref($hash{$h}) =~ /^ARRAY/) {
-                        &nprintf("%8s: %-10s = %s\n", $id, $h, join(",", sort @{$hash{$h}}));
+                        my @scalars;
+                        foreach my $e (@{$hash{$h}}) {
+                            if (ref($e) =~ /^Warewulf::DSO::([a-zA-Z0-9\-_]+)/) {
+                                push(@scalars, lc($1) ."(". ($e->get("name") || "undef") .")");
+                            } else {
+                                push(@scalars, $e);
+                            }
+                        }
+                        if (scalar(@scalars) > 0) {
+                            printf("%8s: %-10s = %s\n", $id, $h, join(",", sort @scalars));
+                        }
                     } else {
-                        &nprintf("%8s: %-10s = %s\n", $id, $h, $hash{$h});
+                        if (ref($e) =~ /^Warewulf::DSO::([a-zA-Z0-9\-_]+)/) {
+                            printf("%8s: %-10s = %s\n", $id, $h, lc($1) ."(". ($e->get("name") || "undef") .")");
+                        } else {
+                            printf("%8s: %-10s = %s\n", $id, $h, $hash{$h});
+                        }
                     }
                 }
             } else {
@@ -183,9 +247,179 @@ exec()
                         push(@values, $o->get($g) || "UNDEF");
                     }
                 }
-                &nprintf("%-20s " x (scalar @values) ."\n", @values);
+                printf("%-15s " x (scalar @values) ."\n", @values);
             }
         }
+    } elsif ($command eq "set" or $command eq "edit" or $command eq "create") {
+        &dprint("Entered 'set' loop\n");
+        my $persist_bool;
+
+        if ($opt_netdev) {
+            &dprint("Working on network device: $opt_netdev\n");
+            foreach my $obj ($objSet->get_list()) {
+                my $netobject;
+                &dprint("looking for an existing network device name: $opt_netdev\n");
+                foreach my $nobj ($obj->get("netdevs")) {
+                    &dprint("testing match for: ". $nobj->get("name") ."\n");
+                    if ($nobj->get("name") eq $opt_netdev) {
+                        &dprint("Using existing netdev object: $nobj\n");
+                        $netobject = $nobj;
+                        last;
+                    }
+                }
+                if ($opt_devremove) {
+                    if ($netobject) {
+                        $obj->del("hwaddr", $netobject->get("hwaddr"));
+                        $obj->del("netdevs", $netobject);
+                        $persist_bool = 1;
+                    } else {
+                        &wprint("No device named '$opt_netdev' found\n");
+                    }
+                } else {
+                    if (! $netobject) {
+                        &dprint("Creating new netdev object\n");
+                        $netobject = Warewulf::DSO::Netdev->new();
+                        $netobject->set("name", $opt_netdev);
+                        $obj->add("netdevs", $netobject);
+                    }
+                    if ($opt_ipaddr) {
+                        $netobject->set("ipaddr", $opt_ipaddr);
+                        $persist_bool = 1;
+                    }
+                    if ($opt_netmask) {
+                        $netobject->set("netmask", $opt_netmask);
+                        $persist_bool = 1;
+                    }
+                    if ($opt_hwaddr) {
+                        $netobject->set("hwaddr", $opt_hwaddr);
+                        $obj->add("hwaddr", $opt_hwaddr);
+                        $persist_bool = 1;
+                    }
+                }
+            }
+
+            if ($opt_devremove) {
+                push(@changes, sprintf("   %10s = %s\n", $opt_netdev, "REMOVE"));
+            } else {
+                if ($opt_ipaddr) {
+                    push(@changes, sprintf("   %10s = %s\n", "$opt_netdev.IPADDR", $opt_ipaddr));
+                }
+                if ($opt_netmask) {
+                    push(@changes, sprintf("   %10s = %s\n", "$opt_netdev.NETMASK", $opt_netmask));
+                }
+                if ($opt_hwaddr) {
+                    push(@changes, sprintf("   %10s = %s\n", "$opt_netdev.HWADDR", $opt_hwaddr));
+                }
+            }
+        }
+
+        if ($opt_bootstrap) {
+            if (uc($opt_bootstrap) eq "UNDEF") {
+                foreach my $obj ($objSet->get_list()) {
+                    my $name = $obj->get("name") || "UNDEF";
+                    $obj->del("bootstrap");
+                    &dprint("Deleting bootstrap for node name: $name\n");
+                    $persist_bool = 1;
+                }
+                push(@changes, sprintf("   %10s = %s\n", "BOOTSTRAP", "UNDEF"));
+            } else {
+                foreach my $obj ($objSet->get_list()) {
+                    my $name = $obj->get("name") || "UNDEF";
+                    $obj->set("bootstrap", $opt_bootstrap);
+                    &dprint("Setting bootstrap for node name: $name\n");
+                    $persist_bool = 1;
+                }
+                push(@changes, sprintf("   %10s = %s\n", "BOOTSTRAP", $opt_bootstrap));
+            }
+        }
+
+        if ($opt_vnfs) {
+            if (uc($opt_vnfs) eq "UNDEF") {
+                foreach my $obj ($objSet->get_list()) {
+                    my $name = $obj->get("name") || "UNDEF";
+                    $obj->del("vnfs");
+                    &dprint("Deleting vnfs for node name: $name\n");
+                    $persist_bool = 1;
+                }
+                push(@changes, sprintf("   %10s = %s\n", "VNFS", "UNDEF"));
+            } else {
+                foreach my $obj ($objSet->get_list()) {
+                    my $name = $obj->get("name") || "UNDEF";
+                    $obj->set("vnfs", $opt_vnfs);
+                    &dprint("Setting vnfs for node name: $name\n");
+                    $persist_bool = 1;
+                }
+                push(@changes, sprintf("   %10s = %s\n", "VNFS", $opt_vnfs));
+            }
+        }
+
+        if (@opt_set) {
+            foreach my $setstring (@opt_set) {
+                my ($key, @vals) = &quotewords('=', 1, $setstring);
+                &dprint("Set: setting $key to ". join("=", @vals) ."\n");
+                foreach my $obj ($objSet->get_list()) {
+                    $obj->set($key, &quotewords(',', 0, join("=", @vals)));
+                }
+                $persist_bool = 1;
+            }
+        }
+        if (@opt_add) {
+            foreach my $setstring (@opt_add) {
+                my ($key, @vals) = &quotewords('=', 1, $setstring);
+                foreach my $val (&quotewords(',', 0, join("=", @vals))) {
+                    &dprint("Set: adding $key to $val\n");
+                    foreach my $obj ($objSet->get_list()) {
+                        $obj->add($key, split(",", $val));
+                    }
+                    $persist_bool = 1;
+                }
+            }
+
+        }
+        if (@opt_del) {
+            foreach my $setstring (@opt_del) {
+                my ($key, @vals) = &quotewords('=', 1, $setstring);
+                if ($key and @vals) {
+                    foreach my $val (&quotewords(',', 0, join("=", @vals))) {
+                        &dprint("Set: deleting $val from $key\n");
+                        foreach my $obj ($objSet->get_list()) {
+                            $obj->del($key, split(",", $val));
+                        }
+                        $persist_bool = 1;
+                    }
+                } elsif ($key) {
+                    &dprint("Set: deleting $key\n");
+                    foreach my $obj ($objSet->get_list()) {
+                        $obj->del($key);
+                    }
+                    $persist_bool = 1;
+                }
+            }
+        }
+
+        if ($persist_bool) {
+            if ($term->interactive()) {
+                print "Are you sure you want to make the following changes to ". scalar($objSet->get_list()) ." node(s):\n\n";
+                foreach my $change (@changes) {
+                    print $change;
+                }
+                print "\n";
+                my $yesno = lc($term->get_input("Yes/No> ", "no", "yes"));
+                if ($yesno ne "y" and $yesno ne "yes") {
+                    &nprint("No update performed\n");
+                    return();
+                }
+            }
+
+            $return_count = $db->persist($objSet);
+
+            &iprint("Updated $return_count objects\n");
+        }
+
+
+    } else {
+        &eprint("Unknown command: $command\n");
+        return;
     }
 
     # We are done with ARGV, and it was internally modified, so lets reset
