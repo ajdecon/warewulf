3bb65b3ebe720a58697c0698a39b4967ac9a5549
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-04-29 06:00:55 +0000

    Update vnfs.pl to use 'vnfsid' as well as using the CGI->print method.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@357 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- provision/cgi-bin/vnfs.pl
+++ provision/cgi-bin/vnfs.pl
@@ -26,49 +26,52 @@ if ($q->param('hwaddr')) {
         my $nodeSet = $db->get_objects("node", "hwaddr", $hwaddr);
         my $node = $nodeSet->get_object(0);
         if ($node) {
-            my ($vnfs) = $node->get("vnfs");
-            if ($vnfs) {
-                my $obj = $db->get_objects("vnfs", "name", $vnfs)->get_object(0);
+            my ($node_name) = $node->get("name");
+            my ($vnfsid) = $node->get("vnfsid");
+            if ($vnfsid) {
+                my $obj = $db->get_objects("vnfs", "id", $vnfsid)->get_object(0);
                 if ($obj) {
-                    print "Content-Type:application/octet-stream; name=\"vnfs.img\"\r\n";
+                    my ($vnfs_name) = $obj->get("name");
+                    &nprint("Sending VNFS '$vnfs_name' to node '$node_name'\n");
+                    $q->print("Content-Type:application/octet-stream; name=\"vnfs.img\"\r\n");
                     if (my $size = $obj->get("size")) {
-                        print "Content-length: $size\r\n";
+                        $q->print("Content-length: $size\r\n");
                     }
-                    print "Content-Disposition: attachment; filename=\"vnfs.img\"\r\n";
-                    print "\r\n";
+                    $q->print("Content-Disposition: attachment; filename=\"vnfs.img\"\r\n");
+                    $q->print("\r\n");
                     my $binstore = $db->binstore($obj->get("id"));
                     while(my $buffer = $binstore->get_chunk()) {
-                        print $buffer;
+                        $q->print($buffer);
                     }
                 } else {
                     &eprint("VNFS request for an unset VNFS\n");
-                    print "Content-Type:application/octet-stream\r\n";
-                    print "Status: 404\r\n";
-                    print "\r\n";
+                    $q->print("Content-Type:application/octet-stream\r\n");
+                    $q->print("Status: 404\r\n");
+                    $q->print("\r\n");
                 }
             } else {
                 &eprint($node->get("name") ." has no VNFS set\n");
-                print "Content-Type:application/octet-stream\r\n";
-                print "Status: 404\r\n";
-                print "\r\n";
+                $q->print("Content-Type:application/octet-stream\r\n");
+                $q->print("Status: 404\r\n");
+                $q->print("\r\n");
             }
         } else {
             &eprint("VNFS request for an unknown node\n");
-            print "Content-Type:application/octet-stream\r\n";
-            print "Status: 404\r\n";
-            print "\r\n";
+            $q->print("Content-Type:application/octet-stream\r\n");
+            $q->print("Status: 404\r\n");
+            $q->print("\r\n");
         }
     } else {
         &eprint("VNFS request for a bad hwaddr\n");
-        print "Content-Type:application/octet-stream\r\n";
-        print "Status: 404\r\n";
-        print "\r\n";
+        $q->print("Content-Type:application/octet-stream\r\n");
+        $q->print("Status: 404\r\n");
+        $q->print("\r\n");
     }
 } else {
     &eprint("VNFS request without a hwaddr\n");
-    print "Content-Type:application/octet-stream\r\n";
-    print "Status: 404\r\n";
-    print "\r\n";
+    $q->print("Content-Type:application/octet-stream\r\n");
+    $q->print("Status: 404\r\n");
+    $q->print("\r\n");
 }
 
 
