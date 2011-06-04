ac9ed9f31d1dcf237358b861f56a70791d292b38
mej <mej@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-06-04 03:13:13 +0000

    Fri Jun  3 20:12:58 2011                        Michael Jennings (mej)
    Initial work on cleaning up the autotools stuff.  Still more can be
    done here though.
    ----------------------------------------------------------------------
    
    
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@411 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- provision/autogen.sh
+++ provision/autogen.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
-set -x
-libtoolize -f -c
-aclocal
-autoheader
-autoconf
-automake -ca -Wno-portability
+if autoreconf -V >/dev/null 2>&1 ; then
+    set -x
+    autoreconf -f -i
+else
+    set -x
+    aclocal
+    autoconf
+    automake -ca
+fi
 
 if [ -z "$NO_CONFIGURE" ]; then
    ./configure $@
