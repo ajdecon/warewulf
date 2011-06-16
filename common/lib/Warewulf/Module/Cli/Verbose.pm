d59fcb94338e628add454a283a728ee336de1cdf
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-06-16 19:43:03 +0000

    Seperate output command into particular functions.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@463 3fa55bd7-8392-0410-b022-a3fc164e7bfc

---
+++ common/lib/Warewulf/Module/Cli/Verbose.pm
@@ -0,0 +1,61 @@
+#
+# Copyright (c) 2001-2003 Gregory M. Kurtzer
+#
+# Copyright (c) 2003-2011, The Regents of the University of California,
+# through Lawrence Berkeley National Laboratory (subject to receipt of any
+# required approvals from the U.S. Dept. of Energy).  All rights reserved.
+#
+
+
+package Warewulf::Module::Cli::Verbose;
+
+use Warewulf::Logger;
+
+our @ISA = ('Warewulf::Module::Cli');
+
+
+sub
+new()
+{
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $self = {};
+
+    bless($self, $class);
+
+    return $self;
+}
+
+sub
+exec()
+{
+    my $self = shift;
+    my $debug_level = uc(shift);
+
+    &set_log_level("INFO");
+
+    &iprint("Debug level: ". &get_log_level() ."\n");
+}
+
+
+sub
+complete()
+{
+    my ($self) = @_;
+
+    return;
+}
+
+
+sub
+summary()
+{
+    my $output;
+
+    $output .= "Increase verbosity of output";
+
+    return($output);
+}
+
+
+1;
