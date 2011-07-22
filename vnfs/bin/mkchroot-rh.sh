c31e88fc11d44177352749a248d054af8c5d0a31
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-07-22 19:24:59 +0000

    Various fixups for ipmi automatic configuration.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@539 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- vnfs/bin/mkchroot-rh.sh
+++ vnfs/bin/mkchroot-rh.sh
@@ -26,14 +26,14 @@ cp -rap /etc/yum.conf /etc/yum.repos.d $VNFSDIR/etc
 sed -i -e "s/\$releasever/$VERSION/g" `find $VNFSDIR/etc/yum* -type f`
 
 echo "Starting package installation"
-yum --installroot $VNFSDIR -y install \
+yum --tolerant --installroot $VNFSDIR -y install \
     SysVinit basesystem bash redhat-release chkconfig coreutils e2fsprogs \
     ethtool filesystem findutils gawk grep initscripts iproute iputils \
     mingetty mktemp net-tools nfs-utils pam portmap procps psmisc rdate \
     sed setup shadow-utils sysklogd tcp_wrappers termcap tzdata util-linux \
     words zlib tar less gzip which util-linux module-init-tools udev \
     openssh-clients openssh-server passwd dhclient pciutils vim-minimal \
-    shadow-utils strace
+    shadow-utils strace vixie-cron cronie crontabs
 
 if [ $? -ne 0 ]; then
     echo "ERROR: Failed to create chroot"
