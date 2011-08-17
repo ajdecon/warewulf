8e08bbba0f4c02defd0294e6ccc2c299d1efc26f
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2011-08-17 00:34:28 +0000

    Additional updates such that processes can be automatically killed after a given time period and thus track properly.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@593 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- common/lib/Warewulf/ParallelCmd.pm
+++ common/lib/Warewulf/ParallelCmd.pm
@@ -27,10 +27,13 @@ Warewulf::ParallelCmd - A parallel command implementation library for Warewulf.
 
     my $o = Warewulf::ParallelCmd->new();
     $o->fanout(4);
-    $o->queue("echo 'long1 start'; sleep 20; echo 'long1 done'");
-    for (my $i=1; $i<= 20; $i++) {
-        $o->queue("echo '$i start'; sleep 4; echo '$i done'");
+    $o->wtime(8);
+    $o->ktime(10);
+    $o->queue("ping -c 20 www.yahoo.com");
+    for (my $i=1; $i<= 4; $i++) {
+        $o->queue("sleep 5");
     }
+    $o->queue("ping -c 20 www.google.com");
     $o->run();
 
 =head1 DESCRIPTION
@@ -70,6 +73,7 @@ init()
 
     $self->set("select", $select);
     $self->set("queueset", $queueset);
+    $self->set("fanout", 64);
 
     return($self);
 }
@@ -105,16 +109,18 @@ fanout($$)
 {
     my ($self, $fanout) = @_;
 
-    $self->set("fanout", $fanout);
+    if ($fanout and $fanout =~ /^([0-9]+)$/) {
+        $self->set("fanout", $1);
+    }
 
-    return;
+    return $self->get("fanout");
 }
 
 
 =item wtime()
 
 How many seconds to wait before throwing a warning to the user that a command is
-still running. (DEFAULT=15)
+still running. If undefined, no warning will be given.
 
 =cut
 
@@ -123,9 +129,52 @@ wtime($$)
 {
     my ($self, $wtime) = @_;
 
-    $self->set("wtime", $wtime);
+    if ($wtime and $wtime =~ /^([0-9]+)$/) {
+        $self->set("wtime", $1);
+    }
 
-    return;
+    return $self->get("wtime");
+}
+
+
+=item ktime()
+
+How many seconds to wait before killing the processes. If undefined, the
+process will wait indefinitely.
+
+=cut
+
+sub
+ktime($$)
+{
+    my ($self, $ktime) = @_;
+
+    if ($ktime and $ktime =~ /^([0-9]+)$/) {
+        $self->set("ktime", $1);
+    }
+
+    return $self->get("ktime");
+}
+
+
+=item pcount()
+
+How many processes are running
+
+=cut
+
+sub
+pcount($$)
+{
+    my ($self, $increment) = @_;
+
+    if ($increment and $increment =~ /^\+([0-9]+)$/) {
+        $self->set("pcount", $self->get("pcount") + $1);
+    } elsif ($increment and $increment =~ /^\-([0-9]+)$/) {
+        $self->set("pcount", $self->get("pcount") - $1);
+    }
+
+    return $self->get("pcount") || 0;
 }
 
 
@@ -140,14 +189,13 @@ run($)
 {
     my ($self) = @_;
     my $select = $self->get("select");
-    my $fanout = $self->get("fanout") || 64;
     my $queueset = $self->get("queueset");
-    my $totalcount = $queueset->count() - 1;
-    my $queuecount = 0;
+    my $fanout = $self->fanout();
+    my @queueobjects = $queueset->get_list();
 
     # Spawning the initial fanout within the queue
-    for (; $queuecount < $fanout && $queuecount < $totalcount; $queuecount++) {
-        $self->forkobj($queueset->get_object($queuecount));
+    while ($self->pcount() < $fanout && @queueobjects) {
+        $self->forkobj(shift(@queueobjects));
     }
 
     my $timer = 1;
@@ -175,18 +223,15 @@ run($)
 
                 } else {
                     $self->closefh($fh);
-                    # Closed one file handle, so now lets queue the next in the array
-                    # if it exists.
-                    if ($queuecount <= $totalcount) {
-                        $self->forkobj($queueset->get_object($queuecount));
-                        $queuecount++;
-                    }
+                }
+
+                while ($self->pcount() < $fanout && @queueobjects) {
+                    $self->forkobj(shift(@queueobjects));
                 }
             }
-        } else {
-            &dprint("Invoking the timer\n");
-            $self->timer();
         }
+        &dprint("Invoking the timer\n");
+        $self->timer();
     }
 }
 
@@ -214,15 +259,19 @@ forkobj($)
     my $select = $self->get("select");
     my $command = $obj->get("command");
     my $fh;
+    my $pid;
 
     &dprint("Spawning command: $command\n");
-    open($fh, "$command |");
+    $pid = open($fh, "$command |");
     $select->add($fh);
 
     &dprint("Created fileno: ". $fh->fileno() ."\n");
 
+    $obj->set("fh", $fh);
     $obj->set("fileno", $fh->fileno());
     $obj->set("starttime", time());
+    $obj->set("pid", $pid);
+    $self->pcount("+1");
 }
 
 sub
@@ -233,6 +282,8 @@ closefh($)
     my $select = $self->get("select");
     my $fileno = $fh->fileno();
 
+    &dprint("Closing fileno: $fileno\n");
+
     my $obj = $queueset->find("fileno", $fileno);
     if ($obj) {
         &dprint("closing out fileno: $fileno\n");
@@ -241,6 +292,7 @@ closefh($)
         $fh->close();
         $obj->set("done", "1");
         $obj->del("fileno");
+        $self->pcount("-1");
     } else {
         &wprint("Could not resolve fileno: $fileno\n");
     }
@@ -252,19 +304,28 @@ timer($)
     my ($self) = @_;
     my $queueset = $self->get("queueset");
     my $curtime = time();
-    my $wtime = $self->get("wtime") || 15;
+    my $wtime = $self->wtime();
+    my $ktime = $self->ktime();
 
     foreach my $obj ($queueset->get_list()) {
-        my $warntime = $obj->get("starttime") + $wtime;
+        my $starttime = $obj->get("starttime");
         my $fileno = $obj->get("fileno");
         my $command = $obj->get("command");
         my $warning = $obj->get("warning");
-        if (! $obj->get("done") and ! $warning and $fileno and $curtime > $warntime) {
-            &wprint("Command still in progress ($command)\n");
-            $obj->set("warning", 1);
+        my $pid = $obj->get("pid");
+        if (! $obj->get("done") and $fileno ) {
+            if (! $warning and $wtime and $curtime > ($starttime + $wtime)) {
+                &wprint("Process $pid still running ($command)\n");
+                $obj->set("warning", 1);
+            } elsif ($ktime and $curtime > ($starttime + $ktime)) {
+                &wprint("Killing process $pid ($command)\n");
+                my $fh = $obj->get("fh");
+                kill("TERM", $pid);
+                kill("INT", $pid);
+                kill("KILL", $pid);
+            }
         }
     }
 }
-
 
 1;
