c912f4bf7f853274935ba83af4fcc511c6e0812b
kmuriki <kmuriki@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2012-03-03 01:10:57 +0000

    Fixed the copyright section in these file to include University of California.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@825 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- monitor/src/getstats.h
+++ monitor/src/getstats.h
@@ -1,23 +1,12 @@
 /*
- * Copyright (c) 2003, The Regents of the University of California, through
- * Lawrence Berkeley National Laboratory (subject to receipt of any
+ * Copyright (c) 2001-2003 Gregory M. Kurtzer
+ * 
+ * Copyright (c) 2003-2011, The Regents of the University of California,
+ * through Lawrence Berkeley National Laboratory (subject to receipt of any
  * required approvals from the U.S. Dept. of Energy).  All rights reserved.
  *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2 of the License, or (at your
- * option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
- * or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
- * for more details.
- *
- * The GNU GPL Document can be found at:
- * http://www.gnu.org/copyleft/gpl.html
- *
- * Written By Greg Kurtzer <GMKurtzer@lbl.gov> for the Warewulf Cluster
- * toolkit project (http://warewulf-cluster.org/).
+ * Contributed by Anthony Salgado & Krishna Muriki
+ * Warewulf Monitor (globals.h)
  *
  */
 
