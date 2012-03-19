106ee08fccf310c858385505e7b3d090c86c7e6e
gmk <gmk@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2012-03-19 23:05:09 +0000

    Don't kill SQL if someone reimports config.
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@886 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- common/share/setup.sql
+++ common/share/setup.sql
@@ -1,16 +1,9 @@
-/****************************************************************************/
-/* Drop Existing Tables */
-
-DROP TABLE IF EXISTS lookup;
-DROP TABLE IF EXISTS binstore;
-DROP TABLE IF EXISTS datastore;
-
 
 /****************************************************************************/
 /* Create Base Tables */
 
 
-CREATE TABLE datastore
+CREATE TABLE IF NOT EXISTS datastore
 (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     type VARCHAR(64),
@@ -22,7 +15,7 @@ CREATE TABLE datastore
 ) ENGINE=INNODB;
 
 
-CREATE TABLE binstore
+CREATE TABLE IF NOT EXISTS binstore
 (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     object_id INT UNSIGNED,
@@ -33,7 +26,7 @@ CREATE TABLE binstore
 ) ENGINE=INNODB;
 
 
-CREATE TABLE lookup
+CREATE TABLE IF NOT EXISTS lookup
 (
     id INT UNSIGNED NOT NULL AUTO_INCREMENT UNIQUE,
     object_id INT UNSIGNED,
@@ -46,36 +39,3 @@ CREATE TABLE lookup
 ) ENGINE=INNODB;
 
 
-/*
-
-INSERT INTO datastore (serialized) VALUES ("serialized data for a group object named: group1");
-INSERT INTO datastore (serialized) VALUES ("serialized data for a group object named: interactive");
-INSERT INTO datastore (serialized) VALUES ("serialized data for a group object named: io");
-INSERT INTO datastore (serialized) VALUES ("serialized data for a rack object named: rack1");
-INSERT INTO datastore (serialized) VALUES ("serialized data for a rack object named: rack2");
-INSERT INTO datastore (serialized) VALUES ("serialized data for a node object named: n0000");
-INSERT INTO datastore (serialized) VALUES ("serialized data for a node object named: n0001");
-INSERT INTO datastore (serialized) VALUES ("serialized data for a node object named: n0002");
-
-INSERT INTO lookup (type, field, value, object_id) VALUES ("group",     "name",     "group1",       "1");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("group",     "name",     "interactive",  "2");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("group",     "name",     "io",           "3");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("rack",      "name",     "rack1",        "4");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("rack",      "name",     "rack2",        "5");
-
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "name",     "n0000",        "6");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "group1",       "6");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "interactive",  "6");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "rack",     "rack1",        "6");
-
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "name",     "n0001",        "7");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "group1",       "7");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "interactive",  "7");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "rack",     "rack1",        "7");
-
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "name",     "n0002",        "8");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "group1",       "8");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "group",    "io",           "8");
-INSERT INTO lookup (type, field, value, object_id) VALUES ("node",      "rack",     "rack2",        "8");
-
-*/
