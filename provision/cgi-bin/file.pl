4644363e26188681016e89dbe598c70b3df68fdf
mej <mej@3fa55bd7-8392-0410-b022-a3fc164e7bfc>
2012-07-27 01:35:56 +0000

    Thu Jul 26 18:35:40 2012                        Michael Jennings (mej)
    Efficiency improvement on file object retrieval.
    ----------------------------------------------------------------------
    
    
    git-svn-id: https://warewulf.lbl.gov/svn/trunk@1034 3fa55bd7-8392-0410-b022-a3fc164e7bfc

--- provision/cgi-bin/file.pl
+++ provision/cgi-bin/file.pl
@@ -39,16 +39,17 @@ if ($hwaddr =~ /^([a-zA-Z0-9:]+)$/) {
 
         if (! $fileid and $node) {
             my $nodeName = $node->name();
+            my @files = $node->get("fileids");
             my %metadata;
 
-            foreach my $file ($node->get("fileids")) {
-                if (! $file) {
-                    next;
-                }
-                my $objSet = $db->get_objects("file", "_id", $file);
+            print $q->header("text/plain");
+            if (scalar(@files)) {
+                my $objSet = $db->get_objects("file", "_id", @files);
+
                 foreach my $obj ($objSet->get_list()) {
                     if ($obj) {
                         my $obj_timestamp = $obj->timestamp() || 0;
+
                         if ($timestamp and $timestamp >= $obj_timestamp) {
                             next;
                         }
@@ -63,10 +64,9 @@ if ($hwaddr =~ /^([a-zA-Z0-9:]+)$/) {
                         );
                     }
                 }
-            }
-            print $q->header("text/plain");
-            foreach my $t (sort {$a <=> $b} keys %metadata) {
-                print $metadata{$t};
+                foreach my $t (sort {$a <=> $b} keys %metadata) {
+                    print $metadata{$t};
+                }
             }
         } elsif ($fileid =~ /^([0-9]+)$/ ) {
             $fileid = $1;
